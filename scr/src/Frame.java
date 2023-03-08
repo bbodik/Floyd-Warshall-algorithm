5e00206a28d3491850dc1aa6443e0fcf9362f434
bohdan <bbodikk06@gmail.com>
2023-03-08 20:09:00 +0200

    do something

--- scr/src/Frame.java
+++ scr/src/Frame.java
@@ -3,6 +3,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+class Grapisc{
+    int cordX,cordY;
+    static int conting;
+
+    public Grapisc(int cordX, int cordY) {
+        this.cordX = cordX;
+        this.cordY = cordY;
+    }
+}
+
 public class Frame extends JFrame {
     boolean isCheck=false;
     int X;
@@ -15,11 +25,12 @@ public class Frame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    public void addCheckingButtons() {
+    public void addCheckingButtons(int x,int y,int tops) {
         JPanel panel = new JPanel(null);
-        panel.setBounds(250, 25, 20*50, 13*50);
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 20; j++) {
+        Grapisc[] graphic=new Grapisc[tops];
+        panel.setBounds(250, 25, x*50, y*50);
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
                 JButton butt = new JButton();
                 butt.setBounds(j*50, i*50, 10, 10);
                 butt.addActionListener(new ActionListener() {
@@ -28,8 +39,9 @@ public class Frame extends JFrame {
                         JButton button = (JButton) e.getSource();
                         if (isCheck) {
                             Graphics g = getGraphics();
-                            g.drawLine(X + 5+7+250, Y + 5+32+25, button.getX() + 5+7+250, button.getY() + 5+32+25);
+                            g.drawLine(X + 262, Y + 62, button.getX() + 262, button.getY() + 62);
                             isCheck = false;
+                            JOptionPane.showInputDialog(panel,"Введіть ваги","Заповнення значення",JOptionPane.QUESTION_MESSAGE);
                         } else {
                             X = button.getX();
                             Y = button.getY();

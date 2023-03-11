cfd53ea6d7c9692e7d2fd3d2949ecef416db5c82
bohdan <bbodikk06@gmail.com>
2023-03-11 15:55:48 +0200

    do maybe the hardest part(the rest is easy i gues)

--- scr/src/Frame.java
+++ scr/src/Frame.java
@@ -29,7 +29,8 @@ class Grapisc {
 
     public static Grapisc[][] addTop(Grapisc[][] graph, Grapisc hiu, Grapisc kaka, int ling) {
         int i = containsPoint(graph[0], hiu), j = containsPoint(graph[0], kaka);
-        if(!Grapisc.isEnded) {
+        System.out.println(i + " " + j);
+        if (!Grapisc.isEnded) {
             if (i == -1) {
                 for (int h = 0; h < graph[0].length; h++) {
                     if (graph[0][h].cordX == -1) {
@@ -54,14 +55,14 @@ class Grapisc {
         if (j == -1 || i == -1) {
             Grapisc.isEnded = true;
         } else {
-            graph[j][i] = new Grapisc(ling);
+            graph[j + 1][i] = new Grapisc(ling);
         }
         return graph;
     }
 
     private static int containsPoint(Grapisc[] graph, Grapisc point) {
         for (int i = 0; i < graph.length; i++) {
-            if (graph[i].equals(point)) {
+            if (graph[i].cordX == point.cordX && graph[i].cordY == point.cordY) {
                 return i;
             }
         }
@@ -72,27 +73,43 @@ class Grapisc {
         int len = graph[0].length;
         int[][] arr = new int[len][len];
         for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {//if(i==j)arr[i][j]=0;
-                arr[i][j] = graph[i+1][j].leng;
-
+            for (int j = 0; j < len; j++) {
+                arr[i][j] = graph[i + 1][j].leng;
+                if (i == j) arr[i][j] = -1;
             }
         }
+        arr=makeSymmetric(arr);
+        System.out.println("-------------------");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println("-------------------");
     }
 
     public static Grapisc[][] generateTwoDArray(Grapisc[][] graph) {
         int len = graph.length;
         for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len-1; j++) {
+            for (int j = 0; j < len - 1; j++) {
                 graph[i][j] = new Grapisc();
             }
         }
         return graph;
+    }
+
+    public static int[][] makeSymmetric(int[][] arr) {
+        int n = arr.length;
+        int[][] symmArr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] != -1) {
+                    symmArr[i][j] = symmArr[j][i] = arr[i][j];
+                }
+            }
+        }
+        return symmArr;
     }
 }
 
@@ -127,8 +144,7 @@ public class Frame extends JFrame {
                                 g.drawLine(X + 262, Y + 62, button.getX() + 262, button.getY() + 62);
                                 isCheck = false;
                                 graphicRef.set(Grapisc.addTop(graphicRef.get(), new Grapisc(X, Y), new Grapisc(button.getX(), button.getY()), Integer.parseInt(JOptionPane.showInputDialog(panel, "Введіть ваги", "Заповнення значення", JOptionPane.QUESTION_MESSAGE))));
-                            }
-                           else {
+                            } else {
                                 Grapisc.Floid(graphicRef.get());
                             }
                         } else {

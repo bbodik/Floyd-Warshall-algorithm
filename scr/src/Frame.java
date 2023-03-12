af0f38e437d43c0b962024526ab9760070a41086
bohdan <bbodikk06@gmail.com>
2023-03-12 21:04:59 +0200

    FINAL i did that shit

--- scr/src/Frame.java
+++ scr/src/Frame.java
@@ -82,7 +82,7 @@ class Grapisc {
 
     public static JTextArea Floid(int[][] arr) {
         JTextArea textArea = new JTextArea();
-        textArea.setBounds(10,50,230,300);
+        textArea.setBounds(10, 50, 230, 230);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 textArea.append(arr[i][j] + "  ");
@@ -92,21 +92,26 @@ class Grapisc {
         return textArea;
 
     }
-    public static JTextArea getFloydWarshallTextArea(int[][] graph) {
+
+    public static JTextArea getFloydWarshallTextArea(int[][] graph, int source) {
         JTextArea textArea = new JTextArea();
         int n = graph.length;
         int[][] dist = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                dist[i][j] = graph[i][j];
+                if (graph[i][j] == -1) {
+                    dist[i][j] = Integer.MAX_VALUE;
+                } else {
+                    dist[i][j] = graph[i][j];
+                }
             }
         }
 
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (dist[i][k] != -1 && dist[k][j] != -1) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
                         dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                     }
                 }
@@ -117,61 +122,25 @@ class Grapisc {
         sb.append("Алгоритм Флойда-Варшла:\n\n");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (dist[i][j] == -1) {
-                    sb.append("INF\t");
+                if (dist[i][j] == Integer.MAX_VALUE) {
+                    sb.append("INF  ");
                 } else {
-                    sb.append(dist[i][j] + "\t");
+                    sb.append(dist[i][j]).append("  ");
                 }
             }
             sb.append("\n");
         }
-        textArea.setText(sb.toString());
-        textArea.setBounds(10,350,230,300);
-        return textArea;
-    }
-    public static JTextArea getDijkstraTextArea(int[][] graph) {
-        JTextArea textArea = new JTextArea();
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-        int[] distance = new int[n];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[0] = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int u = minDistanceVertex(distance, visited);
-            visited[u] = true;
-
-            for (int v = 0; v < n; v++) {
-                if (!visited[v] && graph[u][v] != -1 && distance[u] != Integer.MAX_VALUE &&
-                        distance[u] + graph[u][v] < distance[v]) {
-                    distance[v] = distance[u] + graph[u][v];
-                }
-            }
+        sb.append("\n");
+        sb.append("Алгоритм Дейкстри:\n\n");
+        for (int i = 0; i < dist.length; i++) {
+            sb.append(dist[source][i]).append("  ");
         }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("Dijkstra Algorithm:\n\n");
-        for (int i = 0; i < n; i++) {
-            sb.append(i + " - " + distance[i] + "\n");
-        }
+        sb.append("\n");
         textArea.setText(sb.toString());
-
+        textArea.setBounds(10, 300, 230, 230);
         return textArea;
     }
 
-    private static int minDistanceVertex(int[] distance, boolean[] visited) {
-        int n = distance.length;
-        int minDist = Integer.MAX_VALUE;
-        int minVertex = -1;
-
-        for (int v = 0; v < n; v++) {
-            if (!visited[v] && distance[v] < minDist) {
-                minDist = distance[v];
-                minVertex = v;
-            }
-        }
-
-        return minVertex;
-    }
 
     public static Grapisc[][] generateTwoDArray(Grapisc[][] graph) {
         int len = graph.length;
@@ -216,7 +185,7 @@ public class Frame extends JFrame {
     }
 
     public void addCheckingButtons(int x, int y, int tops) {
-        JFrame frame=this;
+        JFrame frame = this;
         JPanel panel = new JPanel(null);
         AtomicReference<Grapisc[][]> graphicRef = new AtomicReference<>(Grapisc.generateTwoDArray(new Grapisc[tops + 1][tops]));
         panel.setBounds(250, 25, x * 50, y * 50);
@@ -225,14 +194,13 @@ public class Frame extends JFrame {
             if (Grapisc.isEnded) {
                 readyButton.setVisible(false);
                 JLabel lbl1 = new JLabel("Матриця Вагів:");
-                lbl1.setBounds(10,25,230,25);
+                lbl1.setBounds(10, 25, 230, 25);
                 lbl1.setLayout(null);
-                int[][] FinArray=Grapisc.symmetricalArr(graphicRef.get());
+                int[][] FinArray = Grapisc.symmetricalArr(graphicRef.get());
                 frame.add(Grapisc.Floid(FinArray));
                 frame.add(lbl1);
-                frame.add(Grapisc.getFloydWarshallTextArea(FinArray));
-                frame.add(Grapisc.getDijkstraTextArea(FinArray));
-                frame.repaint(10,50,230, frame.getY()-10);
+                frame.add(Grapisc.getFloydWarshallTextArea(FinArray, Integer.parseInt(JOptionPane.showInputDialog(frame, "Введіть початкову вершину\nДля алгоритму дейкстри", "Заповнення значення", JOptionPane.QUESTION_MESSAGE))));
+                frame.repaint(10, 50, 230, frame.getY() - 10);
 
             }
         });
@@ -245,12 +213,12 @@ public class Frame extends JFrame {
                     readyButton.setText("Розрахувати!");
                     break;
                 }
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                System.out.println("mistake in Thread");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("mistake in Thread");
+                }
             }
-        }
         });
         readyButton.setBounds(10, 10, 230, 60);
         frame.add(readyButton);
